bac7bc485f7b9878b39ddee85a072a46b12b110c
Felipe Ribeiro Albernaz <feliperibeiro1998@gmail.com>
2015-07-14 17:45:39 -0300

    Aula dia 14/07 realizado com o aluno Benhur Carlos...

---
+++ Exercicios/aula14.07.15_Benhur-Felipe.R/aula14.07.15_Benhur-Felipe.R/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula14._07._15_Benhur_Felipe.R
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        cachorro fido = new cachorro();
+        fido.tamanhoCm = 95;
+        fido.SetNome("Fido");
+        fido.SetPeso (42.3);
+
+        Console.WriteLine("Nome: {0}", fido.GetNome());
+        Console.WriteLine("Peso: {0}", fido.GetPeso());
+        Console.WriteLine("Tamanho (m): {0}", fido.tamanhoCm);
+
+        Console.WriteLine("Quantidade diaria de ração: {0}", fido.GetRacaoDiariaKg());
+        }
+    }
+}
---
+++ Exercicios/aula14.07.15_Benhur-Felipe.R/aula14.07.15_Benhur-Felipe.R/cachorro.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aula14._07._15_Benhur_Felipe.R
+{
+    class cachorro
+    {
+        private string _nome;
+        private double _peso;
+        public double tamanhoCm;
+        private double _RacaoDiariaKg;
+
+        public void SetNome(string nome)
+        {
+            _nome = nome; 
+        }
+
+        public string GetNome () 
+        {
+            return _nome;
+        }
+
+        public void SetPeso(double peso)
+        {
+            _peso = peso;
+        }
+
+        public double GetPeso()
+        {
+            return _peso;
+        }
+
+        public void SetRacaoDiariaKg(double RDK) 
+        {
+            RDK = (_peso * _peso) / ( tamanhoCm * tamanhoCm);
+            _RacaoDiariaKg = RDK;
+        }
+
+        public double GetRacaoDiariaKg() 
+        {
+            return _RacaoDiariaKg;
+        }
+    }
+}
