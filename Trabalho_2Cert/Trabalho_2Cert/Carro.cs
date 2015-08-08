58521e2bb2b8180532ed16732f69b1bae3a994ff
Felipe Ribeiro <feliperibeiro1998@gmail.com>
2015-08-08 20:27:16 -0300

    Trabalho da segunda certificação

---
+++ Trabalho_2Cert/Trabalho_2Cert/Carro.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_2Cert
+{
+    class Carro
+    {
+        int _id;
+        string _modelo;
+        string _marca;
+        string _placa;
+        int _Numpassageiros;
+
+        public void SetId(int Id)
+        {
+            _id = Id;
+        }
+        public int GetId()
+        {
+            return _id;
+        }
+        public void SetModelo(string modelo)
+        {
+            _modelo = modelo;
+        }
+        public string GetModelo()
+        {
+            return _modelo;
+        }
+        public void SetMarca(string marca)
+        {
+            _marca = marca;
+        }
+        public string GetMarca()
+        {
+            return _modelo;
+        }
+        public void SetPlaca(string placa)
+        {
+            _placa = placa;
+        }
+        public string GetPlaca()
+        {
+            return _placa;
+        }
+        public void SetNumPassageiros(int passageiros)
+        {
+            _Numpassageiros = passageiros;
+        }
+        public int GetNumPassageiros()
+        {
+            return _Numpassageiros;
+        }
+    }
+}
---
+++ Trabalho_2Cert/Trabalho_2Cert/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabalho_2Cert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Carro car = new Carro();
+        List<Carro> carros = new List<Carro>();
+        int PessoasTotal = 0, aux = 0, ncarros = 0, IDpedido, aux2;
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           ncarros = int.Parse(textBox1.Text);
+           textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < ncarros; i++)
+            {
+                
+                
+                car.SetId(i + 1);
+                Console.WriteLine("Escreva o modelo do carro: ");
+                car.SetModelo(textBox2.Text);
+                Console.WriteLine("Escreva a marca do carro: ");
+                car.SetMarca(textBox3.Text);
+                Console.WriteLine("Escreva a placa do carro: ");
+                car.SetPlaca(textBox4.Text);
+                Console.WriteLine("Escreva o numeros de passageiros desse carro: ");
+                aux = int.Parse(textBox5.Text);
+                PessoasTotal = PessoasTotal + aux;
+                car.SetNumPassageiros(aux);
+
+                
+                textBox2.Clear();
+                textBox3.Clear();
+                textBox4.Clear();
+                textBox5.Clear();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            IDpedido = int.Parse(textBox6.Text);
+            
+            while(IDpedido != -1)
+            {
+                car.GetNumPassageiros();
+
+                if (IDpedido == 0 || IDpedido > carros.Count)
+                {
+                    MessageBox.Show("NUMERO INVALIDO!!!");
+                    break;
+                }
+            }
+        }
+    }
+}
