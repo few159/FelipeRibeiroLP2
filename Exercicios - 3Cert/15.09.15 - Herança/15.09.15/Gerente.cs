6384337a3758f046f8babad2f64e8bb6600288f2
Felipe Albernaz <feliperibeiro1998@gmail.com>
2015-09-15 17:02:34 -0300

    Primeira Aula com Herança... 15.09.15

---
+++ Exercicios - 3Cert/15.09.15 - Herança/15.09.15/Gerente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._09._15
+{
+    class Gerente : Funcionario
+    {
+        public string AreaGerenciada { get; set; }
+        public double Adicional { get; set; }
+
+        public double STotal() 
+        {
+            return Salario + Adicional;
+        }
+    }
+}
---
+++ Exercicios - 3Cert/15.09.15 - Sem Herança/15.09.15/Terceirizado.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15._09._15
+{
+    class Terceirizado
+    {
+        public string Nome { get; set; }
+        public int Cpf { get; set; }
+        public DateTime DNascimento { get; set; }
+        public string Matricula { get; set; }
+    }
+}
